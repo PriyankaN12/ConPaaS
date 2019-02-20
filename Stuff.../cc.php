2e53c7cfefbbc5a9b30bf6e74c6c39981bbea25f
PriyankaN12 <priyankanaren96@gmail.com>
2019-02-20 13:27:29 -0800

    Delete cc.php

--- Stuff.../cc.php
+++
@@ -1,8 +0,0 @@
-<?php			//Sends a query to the load balancer, and sends results back.
-	extract($_GET);
-	$header = array("Content-type:application/x-www-form-urlencoded");
-	$request = array('http'=>array('method'=>"POST", 'header'=>$header, 'content'=>$bookname));
-	$context = stream_context_create($request);
-	$retval = file_get_contents("http://localhost:8080",FALSE, $context);
-	echo $retval;
-?>

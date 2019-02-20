d6ef9a096819910d7808683c31b38e740155f1de
PriyankaN12 <priyankanaren96@gmail.com>
2019-02-20 13:28:29 -0800

    Delete sqlDBstage3.sql

--- Stuff.../sqlDBstage3.sql
+++
@@ -1,6 +0,0 @@
-create table books (name varchar(50), author varchar(50), price varchar(5) ,primary key(name));
-
-insert into books(name,author,price) values('Harry Potter','JK Rowling','500');
-insert into books(name,author,price) values('Cracking the Coding Interview','Claire Rhode','999');
-insert into books(name,author,price) values('Percy Jackson','Rick Riordan','250');
-insert into books(name,author,price) values('Kane Chronicles','Rick Riordan','400');
